9063679893a6a1bf3115e99980930cd386edaea0
Nairo Elsner <46656582+nairoelsner@users.noreply.github.com>
2025-01-09 22:25:34 -0300

    fix comment

--- 5/main.py
+++ 5/main.py
@@ -1,6 +1,6 @@
 def reverse_string(s):
     # Em python as strings são imutáveis, caso contrário seria possível 
-    # tratar como uma list de strings e ter uma complexidade de O(n/2) ao invés de O(n)
+    # tratar como uma lista de caracteres e ter uma complexidade de O(n/2) ao invés de O(n)
     reversed = ''
     for i in range(len(s) - 1, -1, -1):
         reversed += s[i]
